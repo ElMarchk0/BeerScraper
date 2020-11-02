e220b53039595cf4f6ab7a3c3820231ec8531574
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2020-11-01 16:26:46 -0800

    text & # of links

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from bs4 import BeautifulSoup
 
 import requests
 
+from collections import Counter
+
+
 response = requests.get("https://justbeerapp.com/guides/ca/bc/victoria/beers")
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
+if response.status_code != 200:
+    	print("Error fetching page")
+else:
+	content = response.content
+
+nb_links = len(soup.find_all('a'))
+
+print(f"There are {nb_links} links in this page")
+
 print(soup.get_text())
+
+
+
+print(soup.title)
 
 
